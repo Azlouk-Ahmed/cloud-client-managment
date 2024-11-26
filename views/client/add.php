cb3c1963531930bc4a62a199816457285593c3d3
azlouk96 <ahmedazlouk9@gmail.com>
2024-11-27 00:14:49 +0100

    a

--- views/client/add.php
+++ views/client/add.php
@@ -1,47 +1,11 @@
-<?php
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Add Client</title>
-    <link rel="stylesheet" href="../../public/styles/index.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
-    <h1>Add Client</h1>
-    <form action="add.php?action=addClient" method="POST">
-        <div class="df">
-
-            <label for="nom">Nom:</label>
-            <input type="text" id="nom" name="nom" required>
-        </div>
-        <div class="df">
-
-            <label for="prenom">Prénom:</label>
-            <input type="text" id="prenom" name="prenom" required>
-        </div>
-
-        <div class="df">
-            <label for="age">Âge:</label>
-            <input type="number" id="age" name="age" required>
-
-        </div>
-        <div class="df">
-
-        </div>
-        <label for="ID_region">Région:</label>
-        <select id="ID_region" name="ID_region" required>
-
-
-
-    </select>
-
-    <button type="submit">Ajouter</button>
-</form>
-
-
-    <br>
-    <a href="list.php">Back to List</a>
+    hello ya zebi
 </body>
 </html>
