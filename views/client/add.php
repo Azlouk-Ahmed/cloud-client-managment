2967505c700894f9e3122527e5b3f01c83069406
azlouk96 <ahmedazlouk9@gmail.com>
2024-11-27 00:27:11 +0100

    dosi

--- views/client/add.php
+++ views/client/add.php
@@ -1,16 +1,51 @@
-
-
+<?php
+echo "PHP is working in add.php";
+?>
 <!DOCTYPE html>
 <html>
-    
 <head>
     <title>Add Client</title>
     <link rel="stylesheet" href="../../public/styles/index.css">
 </head>
 
 <body>
-    <h1>Add Cliente</h1>
-   
+    <h1>Add Client</h1>
+    <form action="add.php?action=addClient" method="POST">
+        <div class="df">
+
+            <label for="nom">Nom:</label>
+            <input type="text" id="nom" name="nom" required>
+        </div>
+        <div class="df">
+
+            <label for="prenom">Prénom:</label>
+            <input type="text" id="prenom" name="prenom" required>
+        </div>
+
+        <div class="df">
+            <label for="age">Âge:</label>
+            <input type="number" id="age" name="age" required>
+
+        </div>
+        <div class="df">
+
+        </div>
+        <label for="ID_region">Région:</label>
+        <select id="ID_region" name="ID_region" required>
+
+
+
+        <?php
+        // Assuming $regionController has been passed to this view and contains a method to fetch regions
+        $regions = $regionController->listRegions();
+        while ($region = $regions->fetch(PDO::FETCH_ASSOC)) {
+            echo "<option value='" . $region['ID_region'] . "'>" . $region['libelle'] . "</option>";
+        }
+        ?>
+    </select>
+
+    <button type="submit">Ajouter</button>
+</form>
 
 
     <br>
